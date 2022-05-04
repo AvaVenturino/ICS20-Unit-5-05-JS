450179a9afce7a032d78af0f373fd7841bca9cc7
Ava Venturino <99355031+AvaVenturino@users.noreply.github.com>
2022-05-04 22:00:27 +0000

    changed names for pwa

--- js/script.js
+++ js/script.js
@@ -8,8 +8,8 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS20-Unit-5-04-JS/sw.js", {
-    scope: "/ICS20-Unit-5-04-JS/",
+  navigator.serviceWorker.register("/ICS20-Unit-5-05-JS/sw.js", {
+    scope: "/ICS20-Unit-5-05-JS/",
   });
 }
 
